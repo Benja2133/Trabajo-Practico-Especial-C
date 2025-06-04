3e37f1198605b88c909834773ceb5f250e6f93ba
Tomas Loyber <tomasloyber@mi.unc.edu.ar>
2025-06-03 22:08:21 -0300

    Arreglo de GCL y Xor

--- main.py
+++ main.py
@@ -1,13 +1,46 @@
-class Generadores: 
-    M: int = (2**31) - 1  
+class GeneradorCongruencialLineal: 
 
-    def congruencial_lineal(self,y: int, a: int = 16807, c: int = 0, m: int = M):
-        return ((a*y) + c) % m
-    
-    def xor_shift_32(self,x:int,a:int = 13,b=17,c=5):
-        x ^= (x << a) & 0xFFFFFFFF
-        x ^= (x >> b)
-        x ^= (x << c) & 0xFFFFFFFF
+    def __init__(self,Y:int ,A: int = 16807, C: int = 0, M: int= (2**31) - 1 ):
+        self.seed = Y
+        self.a = A
+        self.c = C
+        self.m = M
 
-        return x & 0xFFFFFFFF
+    def congruencial_lineal(self):
+        self.seed = (self.a * self.seed + self.c) % self.m
+        return self.seed
+
+
+
+class Xorshift:
+    def __init__(self, semilla):
+        if semilla == 0:
+            raise ValueError("La semilla debe ser distinta de cero para xorshift32")
+        # Asegurar que la semilla esté en el rango de 32bits
+        self.seed = semilla & 0xFFFFFFFF
+
+    def xor_shift_32(self):
+        x = self.seed
+        x ^= (x << 13) & 0xFFFFFFFF # Asegurar que el resultado intermedio sea de 32 bits
+        x ^= (x >> 17) # siempre va a ser un entero de 32 bits
+        x ^= (x << 5) & 0xFFFFFFFF # Asegurar que el resultado intermedio sea de 32 bits
+        self.seed = x
+
+        return x 
     
+
+
+# Pruebas
+if __name__ == "__main__":
+    # Inicializar el estado (debe ser distinto de cero)
+    state = Xorshift(123456789)
+    satate_congruencial = GeneradorCongruencialLineal(123456789)
+    print("Generando 10 números aleatorios con Xorshift:")
+    for _ in range(10):
+        numero_aleatorio = state.xor_shift_32()
+        print(numero_aleatorio)
+
+    print("Generando 10 números aleatorios:")
+    for _ in range(10):
+        numero_aleatorio = Xorshift.xor_shift_32(state)
+        print(numero_aleatorio)
