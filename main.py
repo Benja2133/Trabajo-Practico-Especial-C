24a581224afc5a9c032051988eddabec589df5b2
Tomas Loyber <tomasloyber@mi.unc.edu.ar>
2025-06-13 19:37:33 -0300

    fix simulacion

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ class GCL:
 
     def next(self):
         """
-        Genera el siguiente número aleatorio usando el método del generador congruencial lineal.
+        Genera el siguiente número pseudoaleatorio usando el método del generador congruencial lineal.
         """
         self.state = (self.a * self.state + self.c) % self.m
         return self.state
 
     def random(self):
         """
-        Devuelve un número aleatorio en el rango [0, 1) normalizado.
+        Devuelve un número pseudoaleatorio en el rango [0, 1) normalizado.
         """
         return self.next() / self.m
 
@@ -29,10 +29,11 @@ class Xorshift:
         self.state = seed & 0xFFFFFFFF
 
     def next(self):
+        """Algotimo Xorshift"""
         x = self.state
         x ^= (x << 13) & 0xFFFFFFFF # Asegurar que el resultado intermedio sea de 32 bits
         x ^= (x >> 17) # siempre va a ser un entero de 32 bits
-        x ^= (x << 5) & 0xFFFFFFFF # Asegurar que el resultado intermedio sea de 32 bits
+        x ^= (x << 5) & 0xFFFFFFFF # Asegurar que el resultado sea de 32 bits
         self.state = x
         return x 
 
@@ -90,86 +91,88 @@ class Xoshiro:
         Devuelve un número aleatorio en el rango [0, 1).
         """
         return self.next() / (1 << 64)
-    
+
+
+"""Funciones de utilidad para la simulación""" 
 def lambda_t(t):
-    return 30 + 30 * math.sin(2*math.pi*t / 24)
+    return 30 + 30 * math.sin(2 * math.pi * t / 24)
 
 
 #utiliza adelgazamiento para simular arribos que siguen el proceso de poisson no homogeneo
 def generar_arribos(T, generador,lambda_max=60):
-    Eventos = []
-    t = -math.log(generador.random()) / lambda_max # tiempo entre intentos (vars aleatorias con dist exp(lambda_max))
+    eventos = []
+    x = generador.random()
+    t = -math.log(x) / lambda_max # tiempo entre intentos (vars aleatorias con dist exp(lambda_max))
     while t<=T:
         v = generador.random() 
         if v < lambda_t(t) / lambda_max:
-            Eventos.append(t)
-        t+=-math.log(generador.random()) / lambda_max
+            eventos.append(t)
+        t += -math.log(generador.random()) / lambda_max
     
-    return Eventos
+    return eventos
 
-def simular(generador,T = 48):
-    reloj = 0
-    eventos = []  # cola de arribos
-    tiempo_fuera = 0  # tiempo total que el servidor está detenido
-    lambda_max = 60
+# Generador de tiempos de atención con distribución exponencial
+def generador_atencion(generador, lambda_=40):
+    x = generador.random() 
+    return -math.log(1-x)/lambda_
 
-    eventos = generar_arribos(T,generador,lambda_max)
-    
-    cola = []
+def simular(generador,Horas = 48):
+    arribos = generar_arribos(Horas,generador)
+    print(f"Cantidad de arribos: {len(arribos)}")
+    tiempo_servidor = 0
+    cola_por_hora = [0] * Horas
     i = 0
-    while reloj < T or cola:
-        # Agregamos llegadas a la cola que ocurrieron hasta el tiempo actual
-        while i < len(eventos) and eventos[i] <= reloj:
-            cola.append(eventos[i])
-            i += 1
+    print(f"Simulación con generador: {generador.__class__.__name__}")
 
-        if cola:
-            servicio = -math.log(generador.random()) / 40
-            reloj += servicio
-            cola.pop(0)
+    for arribo in arribos:
+        i += 1
+        t_arribo = int(arribo)
 
-            # Añadimos nuevas llegadas ocurridas durante el servicio
-            while i < len(eventos) and eventos[i] <= reloj:
-                cola.append(eventos[i])
-                i += 1
+        if i in [1, 500, 1000, 1500]: 
+            print(f"Arribo {i}: {arribo:.4f}")
+        if arribo >= tiempo_servidor:
+            tiempo_inicio = arribo
+            if i in [1, 500, 1000, 1500]:
+                print(f"Tiempo de inicio de servicio: {tiempo_inicio:.4f}")
         else:
-            if i < len(eventos):
-                reloj = eventos[i]
-                cola.append(eventos[i])
-                i += 1
-            else:
-               break
+            tiempo_inicio = tiempo_servidor
+            cola_por_hora[t_arribo] += 1
+            if i in [1, 500, 1000, 1500]:
+                print(f"Tiempo de inicio de servicio (con cola): {tiempo_inicio:.4f}")
+        
+        servicio = generador_atencion(generador)
+        if i in [1, 500, 1000, 1500]:
+            print(f"Tiempo de servicio: {servicio:.4f}")
+        tiempo_salida = tiempo_inicio + servicio
+        if i in [1, 500, 1000, 1500]:
+            print(f"Tiempo de salida de servicio: {tiempo_salida:.4f}")
+        tiempo_servidor = tiempo_salida
+        if i in [1, 500, 1000, 1500]:
+            print(f"El cliente {i} fue atendido en la hora {int(tiempo_servidor)}")
+        if i in [1, 500, 1000, 1500]: print("\n")
 
-    return reloj  # aca habria que devolver todos los datos que se piden para hacer la comparacion
+    return {
+    "arribos": arribos,
+    "cola_por_hora": cola_por_hora,
+    "ocupado": tiempo_servidor,
+    "clientes": len(arribos),
+}
+
 
 # Pruebas
 if __name__ == "__main__":
     # Inicializar el estado (debe ser distinto de cero)
     state_xorshift = Xorshift(123456789)
-    state_congruencial = GCL(123456789)
+    state_congruencial = GCL(1234)
     state_xoshiro = Xoshiro(123456789)
 
-    print(simular(state_xorshift))
-    print(simular(state_congruencial))
-    print(simular(state_xoshiro))
+    sim = simular(state_xorshift)
+    sim = simular(state_congruencial)
+    sim = simular(state_xoshiro)
+    print(sim['arribos'])
+    print(f"Tiempo total de simulación: {sim['cola_por_hora']} horas")
+    print(f"Tiempo ocupado por el servidor: {sim['ocupado']:.4f} horas")
+    print(f"Clientes atendidos: {sim['clientes']}")
 
-    '''print("Generando 10 números aleatorios con GCL:")
-    for _ in range(10):
-        numero_aleatorio = satate_congruencial.next()
-        numero_aleatorio_r= satate_congruencial.random()
-        print(f"Número: {numero_aleatorio}, Normalizado: {numero_aleatorio_r}")
-        
-
-    print("Generando 10 números aleatorios con Xorshift:")
-    for _ in range(10):
-        numero_aleatorio = state.next()
-        numero_aleatorio_r = state.random()
-        print(f"Número: {numero_aleatorio}, Normalizado: {numero_aleatorio_r}")
-
-    print("Generando 10 números aleatorios con Xoshiro:")
-    for _ in range(10):
-        numero_aleatorio = state_xoshiro.next()
-        numero_aleatorio_r = state_xoshiro.random()
-        print(f"Número: {numero_aleatorio}, Normalizado: {numero_aleatorio_r}")'''
     
         
