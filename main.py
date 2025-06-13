0af1fd959ba7a835183eae15fb7ba66ffeba0de5
unknown <benjamin.ceballos@mi.unc.edu.ar>
2025-06-13 15:57:35 -0300

    simulacion-del-server-v0

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+
 class GCL: 
 
     def __init__(self,seed:int ,A: int = 16807, C: int = 0, M: int= (2**31) - 1 ):
@@ -88,15 +90,70 @@ class Xoshiro:
         Devuelve un número aleatorio en el rango [0, 1).
         """
         return self.next() / (1 << 64)
+    
+def lambda_t(t):
+    return 30 + 30 * math.sin(2*math.pi*t / 24)
 
+
+#utiliza adelgazamiento para simular arribos que siguen el proceso de poisson no homogeneo
+def generar_arribos(T, generador,lambda_max=60):
+    Eventos = []
+    t = -math.log(generador.random()) / lambda_max # tiempo entre intentos (vars aleatorias con dist exp(lambda_max))
+    while t<=T:
+        v = generador.random() 
+        if v < lambda_t(t) / lambda_max:
+            Eventos.append(t)
+        t+=-math.log(generador.random()) / lambda_max
+    
+    return Eventos
+
+def simular(generador,T = 48):
+    reloj = 0
+    eventos = []  # cola de arribos
+    tiempo_fuera = 0  # tiempo total que el servidor está detenido
+    lambda_max = 60
+
+    eventos = generar_arribos(T,generador,lambda_max)
+    
+    cola = []
+    i = 0
+    while reloj < T or cola:
+        # Agregamos llegadas a la cola que ocurrieron hasta el tiempo actual
+        while i < len(eventos) and eventos[i] <= reloj:
+            cola.append(eventos[i])
+            i += 1
+
+        if cola:
+            servicio = -math.log(generador.random()) / 40
+            reloj += servicio
+            cola.pop(0)
+
+            # Añadimos nuevas llegadas ocurridas durante el servicio
+            while i < len(eventos) and eventos[i] <= reloj:
+                cola.append(eventos[i])
+                i += 1
+        else:
+            if i < len(eventos):
+                reloj = eventos[i]
+                cola.append(eventos[i])
+                i += 1
+            else:
+               break
+
+    return reloj  # aca habria que devolver todos los datos que se piden para hacer la comparacion
 
 # Pruebas
 if __name__ == "__main__":
     # Inicializar el estado (debe ser distinto de cero)
-    state = Xorshift(123456789)
-    satate_congruencial = GCL(123456789)
+    state_xorshift = Xorshift(123456789)
+    state_congruencial = GCL(123456789)
     state_xoshiro = Xoshiro(123456789)
-    print("Generando 10 números aleatorios con GCL:")
+
+    print(simular(state_xorshift))
+    print(simular(state_congruencial))
+    print(simular(state_xoshiro))
+
+    '''print("Generando 10 números aleatorios con GCL:")
     for _ in range(10):
         numero_aleatorio = satate_congruencial.next()
         numero_aleatorio_r= satate_congruencial.random()
@@ -113,5 +170,6 @@ if __name__ == "__main__":
     for _ in range(10):
         numero_aleatorio = state_xoshiro.next()
         numero_aleatorio_r = state_xoshiro.random()
-        print(f"Número: {numero_aleatorio}, Normalizado: {numero_aleatorio_r}")
+        print(f"Número: {numero_aleatorio}, Normalizado: {numero_aleatorio_r}")'''
+    
         
